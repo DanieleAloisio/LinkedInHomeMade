9e77bed03a7682ab110e6e2088f34827caf34610
Daniele Aloisio <daniele.aloisio@reactconsulting.it>
2021-06-18 12:48:52 +0200

    fix grafica

--- LinkedInHomeMade/Startup.cs
+++ LinkedInHomeMade/Startup.cs
@@ -33,8 +33,11 @@ namespace LinkedInHomeMade
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext db)
         {
+
+            db.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
