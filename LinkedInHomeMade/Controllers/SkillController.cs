43231f00a31453af9c19d1eb68f1fcd9b822ec47
Daniele Aloisio <daniele.aloisio@reactconsulting.it>
2021-07-13 12:29:16 +0200

    insert skill

---
+++ LinkedInHomeMade/Controllers/SkillController.cs
@@ -0,0 +1,34 @@
+﻿using Data;
+using Data_Models;
+using LinkedInHomeMade.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkedInHomeMade.Controllers
+{
+    [Authorize]
+    public class SkillController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public SkillController(ApplicationDbContext context, SignInManager<ApplicationUser> signInManager)
+        {
+            _context = context;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public ActionResult InserisciSkill()
+        {
+            return PartialView();
+        }
+
+    }
+}
---
+++ LinkedInHomeMade/Models/SkillViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkedInHomeMade.Models
+{
+    public class SkillViewModel
+    {
+
+        public int Id { get; set; }
+
+        public int IdApplicationUser { get; set; }
+
+        public string Descrizione { get; set; }
+
+        public string Tag { get; set; }
+
+        public int Competenza { get; set; }
+    }
+}
