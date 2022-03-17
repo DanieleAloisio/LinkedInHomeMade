f3fe562773e1e2c580cdf28b0581eff878bac2fa
Aloisio <daniele.aloisio@reactconsulting.it>
2022-03-17 16:43:37 +0100

    rrrr

--- LinkedInHomeMade/Controllers/HomeController.cs
+++ LinkedInHomeMade/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ using System.Threading.Tasks;
 
 namespace LinkedInHomeMade.Controllers
 {
-    [Authorize] //Test EMAIL
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _context;
