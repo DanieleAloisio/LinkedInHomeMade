64012a117671d51c594bea3202ada49b393b3986
Aloisio <daniele.aloisio@reactconsulting.it>
2022-04-27 16:38:58 +0200

    temp

--- LinkedInHomeMade/wwwroot/js/site.js
+++ LinkedInHomeMade/wwwroot/js/site.js
@@ -15,8 +15,36 @@ function getSkills() {
         dataType: 'json',
         success: function (data) {
             if (data) {
-                data.forEach(element => $('#divSectionSkill')
-                    .append(`<small>${element.tag}</small><div class="mb-3 " style="height: 5px"> <input class="col-10" type="range" id="${element.id}" name="${element.id}" min="10" max="100" value="${element.tag}"><button data-value="${element.id}" class="btn btn-outline-danger btn-circle deletable"><i class="fa fa-times"></i></button></div>`));
+                data.forEach(el => {
+
+                    //TODO: Metodo
+                    //let small = document.createElement('small')
+                    //small.innerText = element.tag
+
+                    //let div = document.createElement('div')
+                    //div.classList.add('mb-3')
+                    //div.style.height = '5px'
+
+                    //let input = document.createElement('input')
+                    //input.type = 'range'
+                    //input.id = element.id
+
+                    const creaElemento = (element) => {
+                        return `<small>${element.tag}</small>
+                        <div class="mb-3" style="height: 5px">
+                            <input class="col-10" type="range" id="${element.id}" name="${element.id}" min="10" max="100" value="${element.tag}">
+                            <button data-value="${element.id}" class="btn btn-outline-danger btn-circle deletable"><i class="fa fa-times"></i></button>
+                        </div>`}
+
+                    $('#divSectionSkill')
+                        .append(creaElemento(el))
+                });
+
+                $('.deletable').click(function (e) { addRemoveListener(e) });
+            }
+            else {
+                $('#divSectionSkill')
+                    .append('<h6 class="d-flex align-items-center mb-3">Inserisci le tue 3 migliori <i class="material-icons text-info ml-2">Skill</i> </h6>')
             }
         },
         beforeSend: function () {
@@ -26,7 +54,7 @@ function getSkills() {
         complete: function () {
             appendSpinner('divSectionSkill')
             $('#spinner').addClass('d-none')
-        },
+        }
     });
 }
 
